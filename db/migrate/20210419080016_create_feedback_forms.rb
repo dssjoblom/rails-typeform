de245ee8ee790dac059f85402f06b76c2027ebc7
Daniel Sjöblom <daniel@ziney.co>
2022-06-13 18:19:40 +0300

    Remove unnecessary fields

--- db/migrate/20210419080016_create_feedback_forms.rb
+++ db/migrate/20210419080016_create_feedback_forms.rb
@@ -4,9 +4,7 @@ class CreateFeedbackForms < ActiveRecord::Migration[6.1]
       t.string :language
       t.string :trigger
       t.integer :priority, default: 0
-      t.text :typeform_url
-      t.boolean :logged_in_only, default: false
-      t.boolean :logged_in_beta_tester_only, default: false
+      t.text :typeform_id
       t.boolean :enabled, default: true
 
       t.timestamps
--- db/migrate/20210420064847_rename_typeform_url.rb
+++
@@ -1,5 +0,0 @@
-class RenameTypeformUrl < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :feedback_forms, :typeform_url, :typeform_id
-  end
-end
