3ac11699eab242140e2b12e14f85e0c5d3a39af2
Daniel Sjöblom <daniel@ziney.co>
2022-06-14 17:30:15 +0300

    Migration bugfix

--- db/migrate/20220614141620_create_rails_typeform_feedback_form_links.rb
+++ db/migrate/20220614141620_create_rails_typeform_feedback_form_links.rb
@@ -1,7 +1,7 @@
 class CreateRailsTypeformFeedbackFormLinks < ActiveRecord::Migration[7.0]
   def change
     create_table :rails_typeform_feedback_form_links do |t|
-      t.references :feedback_form, foreign_key: true
+      t.references :rails_typeform_feedback_form, foreign_key: true
       t.string :name
       t.text :url
 
