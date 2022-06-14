00245991adcf5ccc04fbbbf555cdf7b6961da50e
Daniel Sjöblom <daniel@ziney.co>
2022-06-14 17:33:03 +0300

    Fix too long index name

--- db/migrate/20220614141620_create_rails_typeform_feedback_form_links.rb
+++ db/migrate/20220614141620_create_rails_typeform_feedback_form_links.rb
@@ -1,7 +1,7 @@
 class CreateRailsTypeformFeedbackFormLinks < ActiveRecord::Migration[7.0]
   def change
     create_table :rails_typeform_feedback_form_links do |t|
-      t.references :rails_typeform_feedback_form, foreign_key: true
+      t.references :rails_typeform_feedback_form, foreign_key: true, index: { name: :rails_typeform_ff_links_index }
       t.string :name
       t.text :url
 
