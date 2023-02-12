82523beebc37b34d0f1e5000213accc71ec3169a
Daniel Sjöblom <daniel@ziney.co>
2023-02-12 16:34:49 +0200

    Comment code in unused model

--- app/models/rails/typeform/feedback_form_link.rb
+++ app/models/rails/typeform/feedback_form_link.rb
@@ -1,8 +1,8 @@
 module Rails::Typeform
   class FeedbackFormLink < ApplicationRecord
-    validates :name, presence: true
-    validates :url, url: true
+    # validates :name, presence: true
+    # validates :url, url: true
 
-    belongs_to :rails_typeform_feedback_form
+    # belongs_to :rails_typeform_feedback_form
   end
 end
